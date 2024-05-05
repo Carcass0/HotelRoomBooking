6e95023a3f132451dd97fa22cbb077d35ee81561
DekkoBraze <greende556@gmail.com>
2024-05-05 15:18:06 +0300

    DatabaseManagment

--- frontend/src/pages/DatabaseManagment/DatabaseManagment.tsx
+++ frontend/src/pages/DatabaseManagment/DatabaseManagment.tsx
@@ -6,11 +6,12 @@ import Slider from '@mui/material/Slider';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import { Button } from '@mui/material';
-
+import TextField from '@mui/material/TextField';
 
 
 function DatabaseManagment() {
   const [data, setData] = useState<string>('')
+  const [sql, setSql] = useState<string>('')
 
   function getData(mode: string) {
     var request = {
@@ -26,7 +27,8 @@ function DatabaseManagment() {
     })
       .then((response) => response.json())
       .then((data) => {
-          setData(data)
+          var stringifiedData = JSON.stringify(data, null, 2)
+          setData(stringifiedData)
         })
       .catch((error) => {
         console.log(error)
@@ -43,10 +45,20 @@ function DatabaseManagment() {
         <Button variant="outlined" sx={{width: '25%', marginBottom: '1%', marginTop: '1%'}} onClick={() => getData('2')}>Показать клиентов</Button>
         <Button variant="outlined" sx={{width: '25%', marginBottom: '1%', marginTop: '1%'}} onClick={() => getData('3')}>Показать резервации</Button>
         {data && (
-          <Box display='inline-block' textAlign='left'>
+          <Box textAlign='left' display='flex' justifyContent='center'>
           <pre>{data}</pre>
         </Box>
         )}
+        <Box sx={{display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+        <TextField 
+                    id="outlined-basic" 
+                    label="SQL" 
+                    variant="outlined" 
+                    sx={{width: '25%'}} inputProps={{ maxLength: 32 }}
+                    value={sql}
+                    onChange={(e) => setSql(e.target.value)}/>
+        <Button variant="outlined" sx={{width: '25%', marginBottom: '1%', marginTop: '1%'}} onClick={() => getData(sql)}>Выполнить SQL</Button>
+        </Box>
     </Box>
   );
 }
